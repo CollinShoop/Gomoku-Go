27abdc70fda425b6e642ba270ea511d8d059100a
Collin Shoop <cws55854@gmail.com>
2021-03-21 14:34:40 -0400

    Added board logic and simple main for early testing

---
+++ board/board.go
@@ -0,0 +1,209 @@
+package board
+
+import "fmt"
+
+var pieceMapping [3]string = [3]string{".", "1", "2"}
+
+type board struct {
+	Rows, Cols                               int
+	pieces                                   []int
+	lastMovePlayer, lastMoveRow, lastMoveCol int
+	isOverCached                             bool
+}
+
+func NewBoard(rows, cols int) *board {
+	b := new(board)
+	b.Rows = rows
+	b.Cols = cols
+	b.pieces = make([]int, rows*cols)
+	b.lastMovePlayer = -1
+	b.lastMoveRow = -1
+	b.lastMoveCol = -1
+	b.isOverCached = false
+	return b
+}
+
+func (b *board) Pieces() []int {
+	piecesCopy := b.pieces
+	return piecesCopy
+}
+
+// Get which player has a piece in a given position
+// It returns a number, -1 for INVALID, 0 for NONE, 1 for PLAYER 1 and 2 for PLAYER 2
+func (b *board) Get(col, row int) int {
+	if col < 0 || row < 0 || col > b.Cols || row > b.Rows {
+		return -1 // panic?
+	}
+	return b.pieces[row*b.Cols+col]
+}
+
+func (b *board) IsOver() bool {
+	if b.isOverCached {
+		return true
+	}
+	if b.lastMovePlayer == -1 {
+		fmt.Printf("isOver: player=%d\n", b.lastMovePlayer)
+		return false
+	}
+	lineCount := b.maxLineCount(b.lastMovePlayer, b.lastMoveCol, b.lastMoveRow)
+	// according to rules of Standard Gomoku, "overline" eg 6+ is illegal and does not count
+	// for Freestyle, it's allowed.
+	b.isOverCached = lineCount >= 5
+	fmt.Printf("isOver: player=%d; lineCount=%d; isOver=%v\n", b.lastMovePlayer, lineCount, b.isOverCached)
+	return b.isOverCached
+}
+
+func (b *board) Move(player, col, row int) {
+	if col < 0 || row < 0 || col > b.Cols || row > b.Rows {
+		return // panic?
+	}
+	if player < 0 || player > 2 {
+		return // panic?
+	}
+
+	fmt.Printf("Move: player=%d; col=%d; row=%d;\n", player, col, row)
+	b.pieces[row*b.Cols+col] = player
+	// trace out in each direction to see if this piece will flip any others
+	// this happens when placing this piece encloses the opponent
+	b.flipCheck(player, col, row)
+
+	b.lastMovePlayer = player
+	b.lastMoveCol = col
+	b.lastMoveRow = row
+}
+
+func (b *board) flipSingle(col, row int) {
+	piece := b.Get(col, row)
+	if piece == -1 || piece == 0 {
+		return
+	}
+	b.pieces[row*b.Cols+col] = (piece % 2) + 1
+}
+
+func (b *board) flipCheck(player, col, row int) {
+	b.directionalFlipCheck(player, col, row, -1, -1)
+	b.directionalFlipCheck(player, col, row, -1, 0)
+	b.directionalFlipCheck(player, col, row, -1, 1)
+	b.directionalFlipCheck(player, col, row, 1, -1)
+	b.directionalFlipCheck(player, col, row, 1, 0)
+	b.directionalFlipCheck(player, col, row, 1, 1)
+	b.directionalFlipCheck(player, col, row, 0, -1)
+	b.directionalFlipCheck(player, col, row, 0, 1)
+}
+
+func (b *board) maxLineCount(player, col, row int) int {
+	return maxInt(
+		b.maxLineCountDirectional(player, col, row, -1, -1), // upper-left and bottom-right
+		b.maxLineCountDirectional(player, col, row, -1, 1),  // upper-right and bottom-left
+		b.maxLineCountDirectional(player, col, row, 0, 1),   // up-down
+		b.maxLineCountDirectional(player, col, row, 1, 0))   // left-right
+}
+
+func maxInt(value int, values ...int) int {
+	max := value
+	for i := 0; i < len(values); i++ {
+		if values[i] > max {
+			max = values[i]
+		}
+	}
+	return max
+}
+
+// maxLineCountDirectional finds the number of player pieces continuously found in a given direction
+// including pieces also in the opposite direction
+func (b *board) maxLineCountDirectional(player, col, row, dcol, drow int) int {
+	count := 1
+	if dcol == 0 && drow == 0 {
+		return count
+	}
+	// go in specified direction
+	for icol, irow := col+dcol, row+drow; ; // start 1 iteration in to avoid flipping the first piece
+
+	icol, irow = icol+dcol, irow+drow {
+		piece := b.Get(icol, irow)
+		if piece == player {
+			count++
+		} else {
+			break
+		}
+	}
+	dcol *= -1
+	drow *= -1
+	// go in the other direction
+	for icol, irow := col+dcol, row+drow; ; // start 1 iteration in to avoid flipping the first piece
+
+	icol, irow = icol+dcol, irow+drow {
+		piece := b.Get(icol, irow)
+		if piece == player {
+			count++
+		} else {
+			break
+		}
+	}
+	return count
+}
+
+// directionalFlipCheck will check for and flip any pieces as a result
+//  of placing a given piece (player) at the col and row specified.
+// This only checks in a single direction, indicated by dcol/drow (ex -1, -1) for upper-left direction.
+func (b *board) directionalFlipCheck(player, col, row, dcol, drow int) {
+	if dcol == 0 && drow == 0 {
+		return
+	}
+	fmt.Printf("directionalFlipCheck: player=%d; col=%d; row=%d; dcol=%d; drow=%d\n", player, col, row, dcol, drow)
+	var tcol, trow int
+	for icol, irow := col, row; ; {
+		icol += dcol
+		irow += drow
+		piece := b.Get(icol, irow)
+		fmt.Printf("\tdirectionalFlipCheck: icol=%d; irow=%d; piece=%d\n", icol, irow, piece)
+		if piece == -1 || piece == 0 {
+			// wall or empty spot means no flip
+			fmt.Printf("\tdirectionalFlipCheck: out of bounds or empty terminated.\n")
+			return
+		}
+		if piece == player {
+			// hit another piece the same as the player, this terminates the trace
+			tcol = icol
+			trow = irow
+			break
+		}
+		// else piece is other player by default, continue
+	}
+	fmt.Printf("\tdirectionalFlipCheck: Starting flipping.\n")
+	// at this point tcol and trow represents the terminal location after tracing
+	// and running into the next matching player piece, so the next step is to flip
+	// all pieces between (col, row) and (tcol, trow)
+	for icol, irow := col+dcol, row+drow; // start 1 iteration in to avoid flipping the first piece
+	icol != tcol || irow != trow; icol, irow = icol+dcol, irow+drow {
+		fmt.Printf("\t\tdirectionalFlipCheck: Flipping col=%d;row=%d.\n", icol, irow)
+		b.flipSingle(icol, irow)
+	}
+}
+
+func (b *board) Print() {
+	fmt.Print("    ")
+	for i := 0; i < b.Cols; i++ {
+		fmt.Printf(" %X ", i)
+	}
+	fmt.Print("  \n    ")
+	for i := 0; i < b.Cols; i++ {
+		fmt.Print("___")
+	}
+	for i := 0; i < b.Cols*b.Rows; i++ {
+		p := b.pieces[i]
+		if i%b.Cols == 0 {
+			if i > 0 {
+				fmt.Print("│")
+			}
+			fmt.Printf("\n%X  │", (i / b.Cols))
+		}
+		fmt.Printf(" %s ", pieceMapping[p])
+	}
+
+	fmt.Print("|\n    ")
+	for i := 0; i < b.Cols; i++ {
+		fmt.Print("‾‾‾")
+	}
+	fmt.Println()
+}
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/collinshoop/gomoku/board"
+)
+
+func main() {
+	b := board.NewBoard(15, 15)
+	b.Print()
+	b.Move(1, 1, 1)
+	b.Move(2, 1, 2)
+	b.Print()
+	b.Move(1, 1, 3)
+	b.Print()
+	b.Move(2, 0, 2)
+	b.Move(2, 0, 0)
+	b.Print()
+	b.Move(2, 2, 2)
+	b.Print()
+	b.IsOver()
+	b.Move(2, 4, 2)
+	b.Print()
+	b.IsOver()
+	b.Move(2, 3, 2)
+	b.Print()
+	b.IsOver()
+}
